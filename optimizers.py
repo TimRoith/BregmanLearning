b54848fa265d61e241017eda6b746425e5a1c946
Tim Roith <44805883+TimRoith@users.noreply.github.com>
2021-04-09 16:30:00 +0200

    Update optimizers.py

--- optimizers.py
+++ optimizers.py
@@ -56,6 +56,27 @@ class LinBreg(torch.optim.Optimizer):
         p_init = p.data.clone()
         return 1/delta * p_init + reg.sub_grad(p_init)
     
+    @torch.no_grad()
+    def evaluate_reg(self):
+        reg_vals = []
+        for group in self.param_groups:
+            group_reg_val = 0.0
+            delta = group['delta']
+            
+            # define regularizer for this group
+            reg = group['reg']
+            
+            # evaluate the reguarizer for each parametr in group
+            for p in group['params']:
+                group_reg_val += reg(p)
+                
+            # append the group reg val
+            reg_vals.append(group_reg_val)
+            
+        return reg_vals
+                
+        
+    
 # ------------------------------------------------------------------------------------------------------    
 class ProxSGD(torch.optim.Optimizer):
     def __init__(self,params,lr=1e-3,reg=reg.reg_none()):
@@ -154,3 +175,4 @@ class AdamBreg(torch.optim.Optimizer):
     def initialize_sub_grad(self,p, reg, delta):
         p_init = p.data.clone()
         return 1/delta * p_init + reg.sub_grad(p_init)
+    
