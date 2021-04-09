b4fd6eec83b0361ab53f285a826cf555f638652f
Tim Roith <44805883+TimRoith@users.noreply.github.com>
2021-04-09 14:36:58 +0200

    Add files via upload

---
+++ models/fully_connected.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+# class Flatten(nn.Module):
+#     def forward(self, x):
+#         return x.view(x.size(0), -1)
+
+class fully_connected(nn.Module):
+    def __init__(self, sizes, act_fn):
+        super(fully_connected, self).__init__()
+        
+        self.act_fn = act_fn
+        layer_list = [nn.Flatten()]
+        for i in range(len(sizes)-1):
+            layer_list.append(nn.Linear(sizes[i], sizes[i+1]))
+            layer_list.append(self.act_fn)
+            
+        self.layers = nn.Sequential(*layer_list)
+        
+        
+    def forward(self, x):
+        return self.layers(x)
---
+++ models/mnist_conv.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+class mnist_conv(nn.Module):
+    def __init__(self, mean = 0.0, std = 1.0):
+        super(mnist_conv, self).__init__()
+        self.act_fn = nn.ReLU
+        self.conv = torch.nn.Conv2d
+        self.linear = torch.nn.Linear
+        self.mean = mean
+        self.std = std
+
+        self.layers1 = []
+        self.layers2 = []
+        self.layers1.append(self.conv(1, 64, 5))
+        self.layers1.append(nn.MaxPool2d(2))
+        self.layers1.append(self.act_fn())
+
+        self.layers1.append(self.conv(64, 64, 5))
+        self.layers1.append(nn.MaxPool2d(2))
+        self.layers1.append(self.act_fn())
+
+        self.layers1.append(nn.Flatten())
+
+        self.layers2.append(self.linear(4 * 4 * 64, 128))
+        self.layers2.append(self.act_fn())
+
+        self.layers2.append(self.linear(128, 10))
+
+        self.layers1 = nn.Sequential(*self.layers1)
+        self.layers2 = nn.Sequential(*self.layers2)
+
+    def forward(self, x):
+        x = (x - self.mean)/self.std
+        x = self.layers1(x)
+        return self.layers2(x)
