e3321b9864ce28a64d62bc215d58821d8c5c7a54
Tim Roith <44805883+TimRoith@users.noreply.github.com>
2021-05-02 18:10:43 +0200

    Update fully_connected.py

--- models/fully_connected.py
+++ models/fully_connected.py
@@ -7,8 +7,10 @@ import torch
 #         return x.view(x.size(0), -1)
 
 class fully_connected(nn.Module):
-    def __init__(self, sizes, act_fn):
+    def __init__(self, sizes, act_fn, mean=0.0, std=1.0):
         super(fully_connected, self).__init__()
+        self.mean = mean
+        self.std = std
         
         self.act_fn = act_fn
         layer_list = [nn.Flatten()]
@@ -20,4 +22,5 @@ class fully_connected(nn.Module):
         
         
     def forward(self, x):
+        x = (x-self.mean)/self.std
         return self.layers(x)
