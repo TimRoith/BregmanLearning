f0ccd8923419dc9a35292f013829f939f39c5894
Leon Bungert <leon.bungert@fau.de>
2021-07-13 16:12:39 +0200

    fixed small bug in node_sparsity

--- models/aux_funs.py
+++ models/aux_funs.py
@@ -181,7 +181,7 @@ def node_sparsity(model):
         if isinstance(m, torch.nn.Linear):
             a = m.weight
             
-            nnz = torch.count_nonzero(torch.norm(a,p=2,dim=1)).item()
+            nnz = torch.count_nonzero(torch.norm(a.data,p=2,dim=1)).item()
             numel_loc = a.shape[0]
             ret.append(nnz/numel_loc)
     #
